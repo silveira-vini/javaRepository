766b8690763780103d84a4785e432ef14e4af155
Vinicius <Vinicius@DESKTOP-68M20E3>
2024-08-06 08:10:36 -0300

    mandando o primeiro commit editado no Eclipse

--- java-training/src/classesEMetodos/ClassesVsObjetos.java
+++ java-training/src/classesEMetodos/ClassesVsObjetos.java
@@ -5,6 +5,8 @@ public class ClassesVsObjetos {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello GitHub");
+		System.out.println("Testando o primeiro commit");
+		
 	}
 	
 }
