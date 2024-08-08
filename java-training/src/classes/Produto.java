7153cd49ba6a3a761d39fa7ea9e9a6605d74bdf3
Vinicius Silveira <vsribeiro87@gmail.com>
2024-08-08 07:56:02 -0300

    Classe upte

--- java-training/src/classes/Produto.java
+++ java-training/src/classes/Produto.java
@@ -4,35 +4,12 @@ public class Produto {
 
 	String nome;
 	double preco;
-	double desconto;
+	static double desconto = 0.25;
 
-// CONSTRUTORES
 	Produto() {
 	}
 
-	Produto(String nomeInicial) {
-		nome = nomeInicial;
-	}
-
-	Produto(String nomeInicial, double precoInicial) {
-		nome = nomeInicial;
-		preco = precoInicial;
-	}
-
-	Produto(String nomeInicial, double precoInicial, double descontoInicial) {
-		nome = nomeInicial;
-		preco = precoInicial;
-		desconto = descontoInicial;
-	}
-//-------------------------------------
-
-	// MÉTODOS
 	double precoComDesconto() {
 		return preco * (1 - desconto);
 	}
-
-	double precoComDesconto(double descontoDoGerente) {
-		return preco * (1 - (desconto + descontoDoGerente));
-	}
-//----------------------------------
 }
