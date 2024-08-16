b364a00d4bae401d5409bb8fbdd5633e37547805
Vinicius Silveira <vsribeiro87@gmail.com>
2024-08-16 07:39:23 -0300

    OO.rel1p1

---
+++ java-training/src/oo/composicao/Carro.java
@@ -0,0 +1,30 @@
+package oo.composicao;
+
+public class Carro {
+
+	Motor motor = new Motor();
+
+	void ligar() {
+		motor.ligado = true;
+	}
+
+	void desligar() {
+		motor.ligado = false;
+	}
+
+	void acelerar() {
+		if (motor.fatorInjecao < 2.5) {
+			motor.fatorInjecao += 0.4;
+		}
+	}
+
+	void freiar() {
+		if (motor.fatorInjecao > 0.3) {
+			motor.fatorInjecao -= 0.4;
+		}
+	}
+
+	boolean estaLigado() {
+		return motor.ligado;
+	}
+}
---
+++ java-training/src/oo/composicao/CarroTeste.java
@@ -0,0 +1,59 @@
+package oo.composicao;
+
+public class CarroTeste {
+
+	public static void main(String[] args) {
+		
+		Carro c1 = new Carro();
+		
+		System.out.println(c1.estaLigado());
+		
+		c1.ligar();
+		System.out.println(c1.estaLigado());
+		
+		System.out.println(c1.motor.giros());
+		
+		c1.acelerar();
+		System.out.println(c1.motor.giros());
+
+		c1.acelerar();
+		System.out.println(c1.motor.giros());
+		
+		c1.acelerar();
+		System.out.println(c1.motor.giros());
+		
+		c1.acelerar();
+		System.out.println(c1.motor.giros());
+		
+		c1.acelerar();
+		c1.acelerar();
+		c1.acelerar();
+		c1.acelerar();
+		c1.acelerar();
+		c1.acelerar();
+		c1.acelerar();
+		
+		System.out.println(c1.motor.giros());
+		
+		c1.freiar();
+		c1.freiar();
+		c1.freiar();
+
+		System.out.println(c1.motor.giros());
+		
+		c1.freiar();
+		c1.freiar();
+		c1.freiar();
+		c1.freiar();
+		c1.freiar();
+		c1.freiar();
+		
+		System.out.println(c1.motor.giros());
+		
+		c1.desligar();
+		
+		System.out.println(c1.motor.giros());
+	}
+	
+	
+}
