9ff3afd00780a1ad91bff8555e539487552de5ff
Vinicius Silveira <vsribeiro87@gmail.com>
2024-08-18 10:47:26 -0300

    Create Ordenacao.java

---
+++ java-training/src/oo/thiagoAguiar/Ordenacao.java
@@ -0,0 +1,16 @@
+package oo.thiagoAguiar;
+
+import java.util.Arrays;
+
+public class Ordenacao {
+	
+	public static void main(String[] args) {
+		
+		int[] numbers = new int[] {10, 5, 65, 13, 78, 54};
+	
+		Arrays.sort(numbers);
+		
+		System.out.println(Arrays.toString(numbers));
+		
+	}
+}
