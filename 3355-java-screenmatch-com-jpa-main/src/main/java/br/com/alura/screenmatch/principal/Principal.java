b27583e8f4c80dfbc7463b55185c6fbea47cd5d2
Vinicius Silveira <vsribeiro87@gmail.com>
2024-10-02 11:27:25 -0300

    starterAplication

--- 3355-java-screenmatch-com-jpa-main/src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ 3355-java-screenmatch-com-jpa-main/src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -20,36 +20,43 @@ public class Principal {
     private ConverteDados conversor = new ConverteDados();
     private final String ENDERECO = "https://www.omdbapi.com/?t=";
     private final String API_KEY = "&apikey=6585022c";
+    List<DadosSerie> dadosSeries = new ArrayList<>();
 
     public void exibeMenu() {
         var menu = """
+                
                 1 - Buscar séries
                 2 - Buscar episódios
-                
-                0 - Sair                                 
-                """;
+                3 - Listar séries pesquisadas
+                0 - Sair""";
+        var opcao = -1;
+        while (opcao != 0) {
+            System.out.println(menu);
+            opcao = leitura.nextInt();
+            leitura.nextLine();
 
-        System.out.println(menu);
-        var opcao = leitura.nextInt();
-        leitura.nextLine();
-
-        switch (opcao) {
-            case 1:
-                buscarSerieWeb();
-                break;
-            case 2:
-                buscarEpisodioPorSerie();
-                break;
-            case 0:
-                System.out.println("Saindo...");
-                break;
-            default:
-                System.out.println("Opção inválida");
+            switch (opcao) {
+                case 1:
+                    buscarSerieWeb();
+                    break;
+                case 2:
+                    buscarEpisodioPorSerie();
+                    break;
+                case 3:
+                    dadosSeries.forEach(System.out::println);
+                    break;
+                case 0:
+                    System.out.println("Saindo...");
+                    break;
+                default:
+                    System.out.println("Opção inválida");
+            }
         }
     }
 
     private void buscarSerieWeb() {
         DadosSerie dados = getDadosSerie();
+        dadosSeries.add(dados);
         System.out.println(dados);
     }
 
@@ -61,7 +68,7 @@ public class Principal {
         return dados;
     }
 
-    private void buscarEpisodioPorSerie(){
+    private void buscarEpisodioPorSerie() {
         DadosSerie dadosSerie = getDadosSerie();
         List<DadosTemporada> temporadas = new ArrayList<>();
 
